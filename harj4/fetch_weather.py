bc04b6702186d984430b8e28c205f53be77be2fa
jry2014 <xzpelaaja966@gmail.com>
2025-11-25 18:28:45 +0000

    harj 4 deploy rest of the files

---
+++ harj4/fetch_weather.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import requests
+import mysql.connector
+from datetime import datetime
+API_KEY = '8b349fb73e10839d6fb33410b5e42962'
+CITY = 'Oulu'
+URL =f'https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric'
+conn = mysql.connector.connect(host='localhost', user='ubuntu',
+password='1234', database='weather_db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (id INT
+AUTO_INCREMENT PRIMARY KEY, city VARCHAR(50), temperature FLOAT, description
+VARCHAR(100), timestamp DATETIME)''')
+response = requests.get(URL)
+data = response.json()
+temp = data['main']['temp']
+desc = data['weather'][0]['description']
+timestamp = datetime.now()
+cursor.execute('INSERT INTO weather_data (city, temperature, description,timestamp) VALUES (%s, %s, %s, %s)', (CITY, temp, desc, timestamp))
+conn.commit()
+cursor.close()
+conn.close()
+print(f'Data tallennettu: {CITY} {temp}°C {desc}')
---
+++ harj4/fetch_weather.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Your project folder
+WORKDIR="/home/ubuntu/cron_assignment"
+cd "$WORKDIR" || exit 1
+# Polku virtuaaliympäristöön
+VENV_DIR="venv"
+# Luo virtuaaliympäristö, jos ei ole olemassa
+if [ ! -d "$VENV_DIR" ]; then
+echo "Luodaan virtuaaliympäristö..."
+python3 -m venv $VENV_DIR 
+fi
+# Aktivoi virtuaaliympäristö
+source $VENV_DIR/bin/activate
+# Asenna riippuvuudet requirements.txt-tiedostosta
+if [ -f "requirements.txt" ]; then
+echo "Asennetaan riippuvuudet..."
+pip install --upgrade pip
+pip install -r requirements.txt
+else
+echo "requirements.txt ei löytynyt!"
+fi
+# Suorita fetch_weather.py
+if [ -f "fetch_weather.py" ]; then 
+echo "Suoritetaan fetch_weather.py..." 
+python fetch_weather.py
+else
+echo "fetch_weather.py ei löytynyt!"
+fi
+echo "Valmis!"
